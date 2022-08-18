eee579b92b3c130154fcb61499506121fe6f2bcb
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2022-08-18 17:56:32 -0300

    Agregado de las rutas de update, delete tareas de la API

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func createTask(w http.ResponseWriter, r *http.Request){
 	var newTask task;
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-			fmt.Fprintln(w, "Inserte values valids")
+			fmt.Fprintf(w, "Inserte valores validos")
 	}
 
 	json.Unmarshal(reqBody, &newTask)
@@ -56,7 +56,7 @@ func createTask(w http.ResponseWriter, r *http.Request){
 
 //creamos la funcion de llamada a Indice de pagina 
 func indexRoute(w http.ResponseWriter, r *http.Request){
-	fmt.Println(w, "Welcome to the Api");
+	fmt.Fprintf(w, "Bienvenido a la API");
 }
 //definimos la funcion de busqueda de tarea
 func getTask(w http.ResponseWriter, r *http.Request)  {
@@ -64,7 +64,7 @@ func getTask(w http.ResponseWriter, r *http.Request)  {
 	taskID, err := strconv.Atoi(vars["id"])
     //en el casod de existencia de error 
 	if err != nil {
-		fmt.Println(w, "Invalid ID")
+		fmt.Fprintf(w, " ID Invalido")
 
 	}
     //si esta todo OK
@@ -76,7 +76,53 @@ func getTask(w http.ResponseWriter, r *http.Request)  {
 	
 }
 }
+//definimos una funcion para la eliminacion de tareas
+func deleteTask(w http.ResponseWriter, r *http.Request) { 
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
 
+	if err!=nil {
+		fmt.Fprintf(w, " ID Invalido")	
+	}
+	//Si esta to OK 
+	for i, task := range tasks{
+		if task.ID == taskID {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			fmt.Fprintf(w, "Eliminando Tareas de %d", task.ID)
+
+	}
+	}
+}
+//funcione para actualizar tareas
+func updateTask(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+	//guaradamos en una variable la tarea a actualizar
+	var updateTask task
+	
+	if err!=nil {
+		fmt.Fprintf(w, " ID Invalido")
+	}
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		fmt.Fprintf(w, "Por favor colocar valores validos")
+	} 
+
+	json.Unmarshal(reqBody, &updateTask)
+
+	for i, task := range tasks{
+			if task.ID == taskID {
+				tasks = append(tasks[:i], tasks[i+1:]...)
+				updateTask.ID = taskID
+				tasks = append(tasks, updateTask)
+
+				fmt.Fprintf(w, "Task %d updated", taskID)							
+			}
+	}
+	
+}
+//funcion main principal 
 func main() {
 	//establecemos el modo estricto de enrutar, con la var router
 	router := mux.NewRouter().StrictSlash(true)
@@ -88,5 +134,9 @@ func main() {
 	router.HandleFunc("/tasks", createTask).Methods("POST")
 	//ruta de busqueda por ID
 	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	//ruta para elimacion de tareas
+	router.HandleFunc("/deleteTasks/{id}", deleteTask).Methods("DELETE")
+	//ruta para la actualizacion de la tareas
+	router.HandleFunc("/updateTask/{id}", updateTask).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":3000", router))
 	}
